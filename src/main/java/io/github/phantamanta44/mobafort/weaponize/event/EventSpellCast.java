bd83e83dcadd1de915a3cf8bc1723009a470495e
Phanta <evandalong@gmail.com>
2016-07-30 18:50:34 -0500

    Spell cast event

---
+++ src/main/java/io/github/phantamanta44/mobafort/weaponize/event/EventSpellCast.java
@@ -0,0 +1,43 @@
+package io.github.phantamanta44.mobafort.weaponize.event;
+
+import io.github.phantamanta44.mobafort.weaponize.weapon.IWeapon;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class EventSpellCast extends Event {
+
+	private static final HandlerList hl = new HandlerList();
+
+	private Player player;
+	private IWeapon.IWeaponInstance spell;
+	private boolean cancelled;
+
+	public EventSpellCast(Player player, IWeapon.IWeaponInstance spell) {
+		this.player = player;
+		this.spell = spell;
+		this.cancelled = false;
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public IWeapon.IWeaponInstance getSpell() {
+		return spell;
+	}
+
+	public void setCancelled(boolean cancelled) {
+		this.cancelled = cancelled;
+	}
+
+	public boolean isCancelled() {
+		return cancelled;
+	}
+
+	@Override
+	public HandlerList getHandlers() {
+		return hl;
+	}
+
+}
--- src/main/java/io/github/phantamanta44/mobafort/weaponize/event/PlayerInteractHandler.java
+++ src/main/java/io/github/phantamanta44/mobafort/weaponize/event/PlayerInteractHandler.java
@@ -1,6 +1,7 @@
 package io.github.phantamanta44.mobafort.weaponize.event;
 
 import io.github.phantamanta44.mobafort.weaponize.weapon.WeaponTracker;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -12,7 +13,12 @@ public class PlayerInteractHandler implements Listener {
 		if (event.getItem() != null) {
 			WeaponTracker.get(event.getPlayer().getUniqueId()).stream()
 					.filter(i -> i.getTemplate().getType().matches(event.getItem()))
-					.forEach(i -> i.onInteract(event));
+					.forEach(i -> {
+						EventSpellCast esc = new EventSpellCast(event.getPlayer(), i);
+						Bukkit.getServer().getPluginManager().callEvent(esc);
+						if (!esc.isCancelled())
+							i.onInteract(event);
+					});
 		}
 	}
 
